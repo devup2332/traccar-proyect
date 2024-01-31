3ccf54805271c714ef5cc6bd9024b7029e35d298
Diego Rojas <devup2332@gmail.com>
2024-01-30 21:37:10 -0500

    Added more tabs4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <meta
-        httpEquiv="Content-Security-Policy"
-      />
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <body>
         <Providers>{children}</Providers>
       </body>
