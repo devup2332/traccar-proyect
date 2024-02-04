f65e76e60fd958a60560241ed600cb350a275b7e
Diego Rojas <devup2332@gmail.com>
2024-02-03 22:01:17 -0500

    master:

--- src/app/api/db.ts
+++ src/app/api/db.ts
@@ -1,5 +1,5 @@
 import mysql from "serverless-mysql";
-import mysql2 from "mysql2/promise";
+// import mysql2 from "mysql2/promise";
 import { environments } from "./environments";
 
 export const conn = mysql({
@@ -11,14 +11,22 @@ export const conn = mysql({
     user: environments.DB_USER,
   },
 });
+//
+// const db = mysql2.createConnection({
+//   host: environments.DB_HOST,
+//   password: environments.DB_PASSWORD,
+//   port: environments.DB_PORT,
+//   database: environments.DB_DATABASE,
+//   user: environments.DB_USER,
+// });
 
-export const generatePool = async () => {
-  return await mysql2.createConnection({
-    host: environments.DB_HOST,
-    password: environments.DB_PASSWORD,
-    port: environments.DB_PORT,
-    database: environments.DB_DATABASE,
-    user: environments.DB_USER,
-  });
-};
-console.log({ environments });
+// export default db;
+// export const generatePool = async () => {
+//   return await mysql2.createConnection({
+//     host: environments.DB_HOST,
+//     password: environments.DB_PASSWORD,
+//     port: environments.DB_PORT,
+//     database: environments.DB_DATABASE,
+//     user: environments.DB_USER,
+//   });
+// };
--- src/app/api/paraderos/route.ts
+++ src/app/api/paraderos/route.ts
@@ -1,12 +1,9 @@
-import { generatePool } from "../db";
+import { conn } from "../db";
 
-const pool = generatePool();
 export const GET = async () => {
-  const response = await pool.query(
+  const response = conn.query(
     "SELECT id,name,description,time_available as date,ROUND(ST_Area(ST_GeomFromText(area,0)) * 1000000000) as area FROM tc_geofences",
   );
-  console.log({ response });
-
   return Response.json({
     status: 200,
     paraderos: response,
