c06afed597b2e308154910e264ae173cf2f97458
Diego Rojas <devup2332@gmail.com>
2024-02-04 01:56:48 -0500

    master: fiex bug

--- src/app/api/paraderos/route.ts
+++ src/app/api/paraderos/route.ts
@@ -1,6 +1,7 @@
-import { conn } from "../db";
+import { generatePool } from "../db";
 
 export const GET = async () => {
+  const conn = await generatePool();
   const response = conn.query(
     "SELECT id,name,description,time_available as date,ROUND(ST_Area(ST_GeomFromText(area,0)) * 1000000000) as area FROM tc_geofences",
   );
