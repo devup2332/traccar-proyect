c4a4c8736f8c0df1515e510cf0248f7d8ac03e61
Diego Rojas <devup2332@gmail.com>
2024-02-03 20:45:54 -0500

    master: New change

--- src/app/api/paraderos/route.ts
+++ src/app/api/paraderos/route.ts
@@ -1,14 +1,13 @@
-import { createConnectionMysql } from "../db";
+import { conn, createConnectionMysql } from "../db";
 
 export const GET = async () => {
-  const conn = await createConnectionMysql();
-  const [results] = await conn.query(
+  const response = await conn.query(
     "SELECT id,name,description,time_available as date,ROUND(ST_Area(ST_GeomFromText(area,0)) * 1000000000) as area FROM tc_geofences",
   );
-  console.log({ results });
+  console.log({ response });
 
   return Response.json({
     status: 200,
-    paraderos: results,
+    paraderos: response,
   });
 };
